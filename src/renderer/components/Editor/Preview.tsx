8310c330f7e2a2a331713159f9211d8e60592bf1
manhtai <vo@manhtai.com>
2022-03-27 17:12:08 +0700

    CSS & link

--- src/renderer/components/Editor/Preview.tsx
+++ src/renderer/components/Editor/Preview.tsx
@@ -2,10 +2,33 @@
 import convertMd from 'renderer/export/convertMd';
 import { AppStateProps } from 'renderer/state/reducer';
 
+import 'katex/dist/katex.min.css';
+
+const interceptClicks = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+  e.preventDefault();
+  if (e.target && 'href' in e.target) {
+    const target = e.target as HTMLAnchorElement;
+    const { href } = target;
+    const hrefStart = href.substring(0, 7);
+    if (hrefStart === 'http://' || hrefStart === 'https:/') {
+      if (window.ipcAPI) {
+        window.ipcAPI.send.openLink(href);
+      } else {
+        window.open(href, '_blank');
+      }
+    }
+  }
+  return false;
+};
+
 const Preview = (props: AppStateProps) => {
   const { state } = props;
   return (
-    <main className="absolute inset-0 p-20 overflow-auto">
+    <main
+      className="absolute inset-0 p-20 overflow-auto"
+      onClick={(e) => interceptClicks(e)}
+      role="presentation"
+    >
       <div
         className="relative prose"
         dangerouslySetInnerHTML={{ __html: convertMd(state.doc) }}
